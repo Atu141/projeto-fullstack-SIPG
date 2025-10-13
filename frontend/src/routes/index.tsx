45f0df021be64deab8a0e7fe424ebc88c99d4e7e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-13 11:47:56 -0300

    Atualização da aula 28, btn Atualizar

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ListarCategorias from "../pages/categorias/listar-categorias";
 import ListarProdutos from "../pages/produtos/listar-produtos";
+import EditarCategorias from "../pages/categorias/editar-categorias";
 
 
 export default function AppRouter(){
@@ -11,6 +12,11 @@ export default function AppRouter(){
             <Route path="/" element={<h1>Página Principal</h1>} />
             <Route path="/categorias" element={<ListarCategorias />} />
             <Route path="/produtos" element={<ListarProdutos />} />
+            <Route
+                path="/categorias/:categotiaId/editar"
+                element={<EditarCategorias/>}
+            />
+            <Route path="*" element={<Navigate to="/"/>}/>
 
         </Routes>
 
